535e7651123d84f0ce31953cfb15754341d7cd3a
DonutzAndCoffee <62204936+DonutzAndCoffee@users.noreply.github.com>
2024-04-02 12:42:41 +0200

    .

--- JavascriptExtensions/button_helper.js
+++ JavascriptExtensions/button_helper.js
@@ -1,20 +1,6 @@
-
-const json_settings = readtextfile('./JavascriptExtensions/Dons_settings.json')
+const json_settings = readtextfile("./DashTemplates/Donutz's iRacing Button Dashbord/JavascriptExtensions/settings.json")
 const settings = JSON.parse(json_settings);
 
-function ld_GetPlayerName() {
-    if ( !settings || !settings.driverName ) {
-        driverName = 0
-    } else {
-        driverName = settings.driverName
-    }
-    var name = $prop('DataCorePlugin.GameData.PlayerName');
-    return ld_formatName(name, driverName)
-}
-
-
-function returnText(screenID, buttonID)
-{
-	let text = settings[$prop('DataCorePlugin.CurrentGame')][screenID][buttonID].label
-	return text;
+function returnSettings() {
+	return settings;
 }
